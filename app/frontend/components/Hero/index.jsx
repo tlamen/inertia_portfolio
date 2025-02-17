d829a47676359ee51090dfc78d93abcfb5a99b47
tlamen <bernardolobo3@gmail.com>
2025-02-17 15:52:17 +1100

    starting hero section

---
+++ app/frontend/components/Hero/index.jsx
@@ -0,0 +1,13 @@
+import { router } from "@inertiajs/react";
+import { Select, Text } from "@switchdreams/ui";
+import React from "react";
+
+const Hero = ({personalInfo}) => {
+  return (
+    <div className="flex w-full h-1/2 pt-2 px-8 items-center">
+      <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cheeseburger.jpg/640px-Cheeseburger.jpg"} />
+    </div>
+  );
+}
+
+export default Hero;
--- app/frontend/pages/home/index.jsx
+++ app/frontend/pages/home/index.jsx
@@ -1,19 +1,14 @@
 import { Button, Text } from "@switchdreams/ui";
 import React from "react";
 import Header from "../../components/Header";
+import Hero from "../../components/Hero";
 
 const Index = ({language, languageOptions, personalInfo}) => {
   return (
     <div className="bg-[#101E42] h-screen w-full text-white">
       <Header language={language} languageOptions={languageOptions} />
-
       <div className="pt-32">
-        {personalInfo.map((info) => (
-          <div key={info.id}>
-            <Text>{info.title}</Text>
-            <Text>{info.description}</Text>
-          </div>
-        ))}
+        <Hero personalInfo={personalInfo}/>
       </div>
     </div>
   );
